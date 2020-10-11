1651b038552a11a891a8f3967ef50876bf52edf1
Droomii <dowoo2594@naver.com>
2020-10-11 17:58:17 +0900

    dark mode

---
+++ fabric.bat
@@ -0,0 +1,2 @@
+cd %~dp0
+node index.ts "E:\dowoodesktop\마음의 소리 KBS2 5부작 HDTV Film x264 720p-Ernie\마음의 소리 E03 161223 HDTV Film x264 720p-Ernie.mp4" no
--- index.ts
+++ index.ts
@@ -2,7 +2,11 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/dark.html');
+});
+
+app.get('/dark', (req, res) => {
+  res.sendFile(__dirname + '/dark.html');
 });
 
 app.get('/host', (req, res) => {
